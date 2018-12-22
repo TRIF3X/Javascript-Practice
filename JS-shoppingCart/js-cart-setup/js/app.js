401ade5a772cd5aeea64f5aecb52e1f9048dd810
TRIF3X <thewizardofcrypto@gmail.com>
2018-12-21 19:44:30 -0600

    add items to cart by traversing the dom, totals can now be seen

--- JS-shoppingCart/js-cart-setup/js/app.js
+++ JS-shoppingCart/js-cart-setup/js/app.js
@@ -22,13 +22,71 @@
                 
                 const item = {};
                 item.img = `img-cart${partialPath}`
-                let name = event.target.parentElement.parentElement.nextElementSibling;
-                
+                let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
 
-                console.log(item)
+                item.name = name;
+
+                let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+
+                //remove dollar sign and white space, starting at index 1
+                let finalPrice = price.slice(1).trim()
+
+                item.price = finalPrice
                 
+                console.log(item)
+
+                const cartItem = document.createElement('div')
+                cartItem.classList.add(
+                    'cart-item', 
+                    'd-flex', 
+                    'justify-content-between',
+                    'text-capitalize', 
+                    'my-3'
+                    );
+
+                cartItem.innerHTML = `
+                <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                <div class="cart-item-text">
+    
+                  <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                  <span>$</span>
+                  <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                </div>
+                <a href="#" id='cart-item-remove' class="cart-item-remove">
+                  <i class="fas fa-trash"></i>
+                </a>
+              </div>`;
+
+              //select cart
+
+              const cart = document.getElementById('cart')
+              const total = document.querySelector('.cart-total-container')
+              cart.insertBefore(cartItem, total)
+              alert('item added to cart')
+              showTotals();
             }
         })
     })
 
+    //show totals
+    function showTotals() {
+        
+        const total = []
+        const items = document.querySelectorAll('.cart-item-price')
+        items.forEach(item => {
+            total.push(parseFloat(item.textContent))
+        })
+
+        const totalMoney = total.reduce((total, item) => {
+            total += item
+            return total;
+        },0)
+
+        //fixed the final ammt to the 2nd decimal point
+        const finalMoney = totalMoney.toFixed(2);
+        
+        document.getElementById('cart-total').textContent = finalMoney;
+        document.querySelector('.item-total').textContent = finalMoney;
+        document.getElementById('item-count').textContent = total.length;
+    }
 })();
